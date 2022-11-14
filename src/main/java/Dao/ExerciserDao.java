aae899107bc4dc03d9a8f40231af70c51dea4f76
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2022-11-14 19:42:36 +0900

    yhj commit

--- src/main/java/Dao/ExerciserDao.java
+++ src/main/java/Dao/ExerciserDao.java
@@ -8,8 +8,27 @@ import main.java.Utils.JDBCUtil;
 public class ExerciserDao {
 	private JDBCUtil jdbcUtil = null;	// JDBCUtil 참조 변수 선언
 
-	public ExerciserDao() {	// 생성자 
+	public ExerciserDao() {	// �깮�꽦�옄 
 		jdbcUtil = new JDBCUtil();		// JDBCUtil 객체 생성 및 활용
+	}
+	
+	//사용자 계정 password를 전달받아 해당 password의 사용자 정보를 삭제하는 메소드
+	public int deleteExerciser(String password) {
+		String query = "DELETE FROM exerciser WHERE password = ?"; // JDBCUtil 에 query 문 설정
+		Object[] param = new Object[] { password };
+		jdbcUtil.setSqlAndParameters(query, param);
+			
+		try {
+			int result = jdbcUtil.executeUpdate();		// delete 문 실행
+			return result;							// delete 에 의해 반영된 레코드 수 반환
+		} catch(Exception e) {
+			jdbcUtil.rollback();
+			e.printStackTrace();
+		} finally {
+			jdbcUtil.commit();
+			jdbcUtil.close();		// ResultSet, PreparedStatement, Connection 반환
+		}
+		return 0;
 	}
 	
 	public void findExerciser(int exerciserId) {
--- src/main/java/Dao/RecordDao.java
+++ src/main/java/Dao/RecordDao.java
@@ -10,6 +10,22 @@ public class RecordDao {
 		jdbcUtil = new JDBCUtil();		// JDBCUtil 객체 생성 및 활용
 	}
 	
-	
-
+	//게시물의 primaryKey인 recordId 전달받아 해당 recordId의 게시물을 삭제하는 메소드
+	public int deleteRecord(int recordId) {
+		String query = "DELETE FROM record WHERE recordId = ?"; // JDBCUtil 에 query 문 설정
+		Object[] param = new Object[] { recordId };
+		jdbcUtil.setSqlAndParameters(query, param);
+				
+		try {
+			int result = jdbcUtil.executeUpdate();		// delete 문 실행
+			return result;							// delete 에 의해 반영된 레코드 수 반환
+		} catch(Exception e) {
+			jdbcUtil.rollback();
+			e.printStackTrace();
+		} finally {
+			jdbcUtil.commit();
+			jdbcUtil.close();		// ResultSet, PreparedStatement, Connection 반환
+		}
+		return 0;
+	}		
 }
